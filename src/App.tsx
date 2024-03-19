a9125df03ffdb7a23c950437bac9ee19d37c4de1
eggborne <mike@mikedonovan.dev>
2024-03-18 17:02:40 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Site Editor 🐪</h1>
+      </header>
+      <main>blargh</main>
+      <footer>footer</footer>
+    </>
+  )
+}
+
+export default App
